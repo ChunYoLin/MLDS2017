b0ab4180bc3ccade1186ca35652776b6cab04901
ChunYoLin <r04922108@csie.ntu.edu.tw>
2017-04-13 13:12:48 +0800

    .

--- hw2/S2VT.py
+++ hw2/S2VT.py
@@ -3,29 +3,33 @@ import numpy as np
 import data_reader
 
 class S2VT_input(object):
-  def __init__(self, frame_data, text_data, name = None):
+  def __init__(self, frame_data, text_data, sent_len, word_id, name = None):
     batch_size = 1
-    num_steps = 42
+    self.frame_len = 80
+    self.sent_len = sent_len
+    self.vocab_size = len(word_id)
     self.batch_size = batch_size
-    self.num_steps = num_steps
-    self.epoch_size = ((len(data) // batch_size) - 1) // num_steps
-    self.input_frame = data_reader._frame_feat()
-    self.input_text, self.targets = data_reader.Data_producer(
-        data, batch_size, num_steps, name = name)
+    self.num_steps = self.frame_len + self.sent_len
+    self.epoch_size = len(frame_data) // batch_size
+    self.input_batch, self.targets_batch = data_reader.Data_producer(
+            frame_data = frame_data, 
+            text_data = text_data, 
+            batch_size = batch_size, 
+            sent_len = sent_len)
 
 class S2VT_model(object):
     def __init__(self, is_training, input_):
         size = 256
         def lstm_cell():
             return tf.contrib.rnn.LSTMCell(size, forget_bias = 0., state_is_tuple = True)
-        vocab_size = 100000
-        frame_len = 80
-        text_len = input_.num_steps
+        frame_len = input_.frame_len
+        sent_len = input_.sent_len
+        vocab_size = input_.vocab_size
         batch_size = input_.batch_size
-        num_steps = frame_len + text_len
+        num_steps = frame_len + sent_len
         # top
         cell_top = lstm_cell()
-        input_frame = tf.placeholder(dtype = tf.float32, shape = [num_steps, 4096])
+        input_frame = input_.input_batch
         top_state_in = cell_top.zero_state(batch_size, tf.float32)
         top_outputs = []
         with tf.variable_scope("top_cell"):
@@ -33,7 +37,7 @@ class S2VT_model(object):
                 if time_step > 0:
                     tf.get_variable_scope().reuse_variables()
                 if time_step < frame_len:
-                    top_input = tf.reshape(input_frame[time_step], [batch_size, 4096])
+                    top_input = input_frame[:, time_step, :]
                 else:
                     top_input = tf.zeros(shape = [batch_size, 4096])
                 (top_output, top_state_out) = cell_top(top_input, top_state_in)
@@ -41,31 +45,32 @@ class S2VT_model(object):
                 top_output_pad = tf.concat(values = [pad, top_output], axis = 1)
                 top_state_in = top_state_out
                 top_outputs.append(top_output_pad)
-        self._top_final_state = top_state_out
-
         #  bot
         cell_bot = lstm_cell()
         bot_state_in = cell_bot.zero_state(batch_size, tf.float32)
+        bot_final_state = cell_bot.zero_state(batch_size, tf.float32)
         bot_outputs = []
         with tf.variable_scope("bot_cell"):
             #  embedding
             with tf.device("cpu:0"):
                 embedding = tf.get_variable(
                         "embedding", [vocab_size, size], dtype = tf.float32)
-                word_vec = tf.nn.embedding_lookup(embedding, input_.input_text)
-                print word_vec.get_shape()
+                word_vec = tf.nn.embedding_lookup(embedding, input_.targets_batch)
+                print word_vec.shape
             for time_step in range(num_steps):
                 if time_step > 0:
                     tf.get_variable_scope().reuse_variables()
                 if time_step < frame_len:
                     bot_input = top_outputs[time_step]
+                    (bot_output, bot_state_out) = cell_bot(bot_input, bot_state_in)
+                    bot_state_in = bot_state_out
                 else:
                     text_input = word_vec[:, time_step - 80, :]
                     top_cell_input = tf.reshape(top_outputs[time_step][0][size:], [1, size])
                     bot_input = tf.concat([text_input, top_cell_input],1)
-                (bot_output, bot_state_out) = cell_bot(bot_input, bot_state_in)
-                bot_state_in = bot_state_out
-                if time_step >= frame_len:
+                    (bot_output, bot_state_out) = cell_bot(bot_input, bot_state_in)
+                    bot_state_in = bot_state_out
+                    bot_final_state = bot_state_out
                     bot_outputs.append(bot_output)
 
         output = tf.reshape(tf.concat(axis = 1, values = bot_outputs), [-1, size])
@@ -83,7 +88,7 @@ class S2VT_model(object):
                 num_classes = vocab_size)
 
         self._cost = cost = tf.reduce_sum(loss) / batch_size
-        self._final_state = bot_state_in
+        self._final_state = bot_final_state
         self._final_logits = logits
         self._final_probs = tf.nn.softmax(logits)
         tvars = tf.trainable_variables()
@@ -144,8 +149,12 @@ def run_epoch(session, model, eval_op = None, verbose = False):
             iters * model.input.batch_size / (time.time() - start_time)))
     return np.exp(costs / iters)
 
-word_to_id = data_reader._build_vocab()
-feat, target_raw = data_reader._target_data()
-text_data, max_len = data_reader._target_data_to_word_id(target_raw, word_to_id)
-train_input = S2VT_input(frame_data = feat, text_data = text_data)
-a = S2VT_model(is_training = True, input_ = train_input)
+f_data, t_data_raw = data_reader._read_data()
+word_id = data_reader._build_word_id()
+t_data_id, sent_len = data_reader._text_data_to_word_id(t_data_raw, word_id)
+train_input = S2VT_input(
+        frame_data = f_data, 
+        text_data = t_data_id, 
+        sent_len = sent_len, 
+        word_id = word_id)
+S2VT_model(is_training = True, input_ = train_input)
--- hw2/data_reader.py
+++ hw2/data_reader.py
@@ -6,7 +6,25 @@ import collections
 import tensorflow as tf
 import numpy as np
 
-def _build_vocab():
+
+def _read_data():
+    feat_path = './MLDS_hw2_data/training_data/feat/'
+    with open("./MLDS_hw2_data/training_label.json") as train:
+        train_json = json.load(train)
+        train_captions = []
+        frame_data = []
+        for i in range(len(train_json)):
+            caption = train_json[i]["caption"][0]
+            train_caption = 'BOS ' + caption[:len(caption) - 1] + ' EOS'
+            train_captions.append(train_caption)
+            video_id = train_json[i]["id"] 
+            feat_file_name = video_id + '.npy'
+            feat = np.load(feat_path + feat_file_name)
+            frame_data.append(feat)
+        frame_data = np.asarray(frame_data, dtype = np.float32)
+    return frame_data, train_captions
+
+def _build_word_id():
     with open("./MLDS_hw2_data/training_label.json") as train:
         train_json = json.load(train)
         words = []
@@ -20,30 +38,12 @@ def _build_vocab():
         dictionary = dict()
         for word, _ in count:
             dictionary[word] = len(dictionary)
-        #  print dictionary
     return dictionary
 
-def _target_data():
-    feat_path = './MLDS_hw2_data/training_data/feat/'
-    with open("./MLDS_hw2_data/training_label.json") as train:
-        train_json = json.load(train)
-        train_captions = []
-        frame_data = {}
-        for i in range(len(train_json)):
-            caption = train_json[i]["caption"][0]
-            train_caption = 'BOS ' + caption[:len(caption) - 1] + ' EOS'
-            train_captions.append(train_caption)
-            video_id = train_json[i]["id"] 
-            feat_file_name = video_id + '.npy'
-            feat = np.load(feat_path + feat_file_name)
-            frame_data[video_id] = feat
-    return frame_data, train_captions
-_target_data()
-
-def _target_data_to_word_id(corpus, word_to_id):
+def _text_data_to_word_id(text_data_raw, word_to_id):
     max_len = 0
     caption_id = []
-    for caption in corpus:
+    for caption in text_data_raw:
         new_caption = []
         caption = caption.split()
         for word in caption:
@@ -51,29 +51,30 @@ def _target_data_to_word_id(corpus, word_to_id):
         caption_id.append(new_caption)
         if len(caption) > max_len:
             max_len = len(caption)
-            
-    train_data = []
+    text_data = []
     for caption in caption_id:
         pad_caption = caption[:]
         for i in range(max_len - len(caption)):
             pad_caption.append(0)
-        for w in pad_caption:
-            train_data.append(w)
-    train_data.append(0)
-    return train_data, max_len
+        text_data.append(pad_caption)
+    return text_data, max_len
+
+def Data_producer(frame_data, text_data, batch_size, sent_len, name = None):
+    with tf.name_scope(name, "Data_producer", [frame_data, text_data, batch_size, sent_len]):
+        data_size = len(frame_data)
+        i = tf.train.range_input_producer(data_size, shuffle = True).dequeue()
         
+        frame_feat_tensor = tf.convert_to_tensor(frame_data, name = "frame_feat_data", dtype = tf.float32)
+        frame_feat_data = tf.strided_slice(frame_feat_tensor, [i, 0, 0], [(i + 1), 80, 4096])
+        frame_feat_data = tf.reshape(frame_feat_data, [80, 4096])
+        frame_feat_data.set_shape([80, 4096])
 
-def Data_producer(frame_data, text_data, batch_size, num_steps, name = None):
-    with tf.name_scope(name, "Data_producer", [frame_data, text_data, batch_size, num_steps]):
-        text_data = tf.convert_to_tensor(text_data, name = "text_data", dtype = tf.int32)
-        data_len = tf.size(text_data)
-        batch_len = data_len // batch_size
-        data = tf.reshape(text_data[0 : batch_size * batch_len], [batch_size, batch_len])
-        epoch_size = (batch_len - 1) // num_steps
-        i = tf.train.range_input_producer(epoch_size, shuffle = False).dequeue()
-        x = tf.strided_slice(data, [0, i * num_steps], [batch_size, (i + 1) * num_steps])
-        x.set_shape([batch_size, num_steps])
-        y = tf.strided_slice(data, [0, i * num_steps + 1], [batch_size, (i + 1) * num_steps + 1])
-        y.set_shape([batch_size, num_steps])
+        text_id_tensor = tf.convert_to_tensor(text_data, name = "text_id_data", dtype = tf.int32)
+        text_id_data = tf.strided_slice(text_id_tensor, [i, 0], [(i + 1), sent_len])
+        text_id_data = tf.reshape(text_id_data, [sent_len])
+        text_id_data.set_shape([sent_len])
 
-        return x, y
+        frame_batch, text_batch = tf.train.batch([frame_feat_data, text_id_data], batch_size = batch_size)
+        return frame_batch, text_batch
+
+
