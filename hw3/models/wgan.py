2e863767e81339995aeeb9096794ebfb8b21e0d4
ChunYoLin <r04922108@csie.ntu.edu.tw>
2017-05-22 18:11:19 +0800

    seperate basic and wgan

--- hw3/models/wgan.py
+++ hw3/models/wgan.py
@@ -114,39 +114,29 @@ class GAN(object):
         
         #---define loss tensor---#
         #  loss of generator
-        self.g_loss = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(
-                labels=tf.ones_like(fi), logits=fi_logits))
+        self.g_loss = tf.reduce_mean(-fi_logits)
         #  loss of discriminator
-        self.d_loss_ri = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(
-                labels=tf.ones_like(ri), logits=ri_logits))
-        self.d_loss_fi = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(
-                labels=tf.zeros_like(fi), logits=fi_logits))
-        self.d_loss_wt = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(
-                labels=tf.zeros_like(wt), logits=wt_logits))
-        self.d_loss_wi = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(
-                labels=tf.zeros_like(wi), logits=wi_logits))
         self.d_loss = (
-            self.d_loss_ri + 
-            self.d_loss_wt + 
-            self.d_loss_fi +
-            self.d_loss_wi
+            tf.reduce_mean(ri_logits) -
+            tf.reduce_mean(fi_logits) -
+            tf.reduce_mean(wt_logits)
+            #  tf.reduce_mean(wi_logits)
             )
         #---seperate the variables of discriminator and generator by name---#
         t_vars = tf.trainable_variables()
         self.d_vars = [var for var in t_vars if 'd_' in var.name]
         self.g_vars = [var for var in t_vars if 'g_' in var.name]
         self.saver = tf.train.Saver()
+
+        #---weight clipping---#
+        self.d_clip = [p.assign(tf.clip_by_value(p, -0.01, 0.01)) for p in self.d_vars]
+
         #---training op---#
         learning_rate_d = 0.00005
         learning_rate_g = 0.00005
-        self.d_optim = tf.train.AdamOptimizer(learning_rate_d).minimize(
-            self.d_loss, var_list=self.d_vars)
-        self.g_optim = tf.train.AdamOptimizer(learning_rate_g).minimize(
+        self.d_optim = tf.train.RMSPropOptimizer(learning_rate_d).minimize(
+            -self.d_loss, var_list=self.d_vars)
+        self.g_optim = tf.train.RMSPropOptimizer(learning_rate_g).minimize(
             self.g_loss, var_list=self.g_vars)
 
     def train(self):
@@ -160,15 +150,11 @@ class GAN(object):
                 -1, 1, [self.batch_size, self.z_dim]).astype(np.float32)
             print "--------------------------------"
             print "epoch {}".format(epoch)
-            d_loss, _, _ = sess.run(
-                [self.d_loss, self.d_optim, self.d_clip],
-                feed_dict={self.z: batch_z, self.keep_prob: 1.0}
-                )
-            real_imgs, sample_imgs, g_loss, _ = sess.run(
-                [self.img_batch_flip, self.fake_image_batch, self.g_loss, 
-                self.g_optim], 
-                feed_dict={self.z: batch_z, self.keep_prob: 1.0}
-                )
+            for _ in range(5):
+                d_loss, _, _ = sess.run(
+                    [self.d_loss, self.d_optim, self.d_clip],
+                    feed_dict={self.z: batch_z, self.keep_prob: 1.0}
+                    )
             real_imgs, sample_imgs, g_loss, _ = sess.run(
                 [self.img_batch_flip, self.fake_image_batch, self.g_loss, 
                 self.g_optim], 
